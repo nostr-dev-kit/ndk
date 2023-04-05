8d0df38a2eba2faef1a5341122451fe73b0be68a
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-04-05 12:21:29 +0100

    Adding other known kinds

--- src/events/kind.ts
+++ src/events/kind.ts
@@ -5,7 +5,9 @@ export declare enum NDKKind {
     Contacts = 3,
     EncryptedDirectMessage = 4,
     EventDeletion = 5,
+    Repost = 6,
     Reaction = 7,
+    BadgeAward = 8,
     ChannelCreation = 40,
     ChannelMetadata = 41,
     ChannelMessage = 42,
@@ -14,7 +16,15 @@ export declare enum NDKKind {
     Report = 1984,
     ZapRequest = 9734,
     Zap = 9735,
+    MuteList = 10000,
+    PinList = 10001,
     RelayList = 10002,
     ClientAuth = 22242,
-    Article = 30023
+    NostrConnect = 24133,
+    CategorizedPeopleList = 30000,
+    CategorizedBookmarkList = 30001,
+    ProfileBadge = 30008,
+    BadgeDefinition = 30009,
+    Article = 30023,
+    AppSpecificData = 30078
 }

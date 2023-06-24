76ce4c3499ab2f4c37bf884cce997c283b04b159
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-06-24 11:33:50 -0400

    Add ability to set custom fields and tests

--- src/user/profile.ts
+++ src/user/profile.ts
@@ -21,19 +21,48 @@ export interface NDKUserProfile {
 export function mergeEvent(event: NDKEvent, profile: NDKUserProfile): NDKUserProfile {
     const payload = JSON.parse(event.content);
 
-    if (payload.name) profile.name = payload.name;
-    if (payload.display_name) profile.displayName = payload.display_name;
-    if (payload.displayName) profile.displayName = payload.displayName;
-    if (payload.image) profile.image = payload.image;
-    if (payload.picture) profile.image = payload.picture;
-    if (payload.banner) profile.banner = payload.banner;
-    if (payload.bio) profile.bio = payload.bio;
-    if (payload.nip05) profile.nip05 = payload.nip05;
-    if (payload.lud06) profile.lud06 = payload.lud06;
-    if (payload.lud16) profile.lud16 = payload.lud16;
-    if (payload.about) profile.about = payload.about;
-    if (payload.zapService) profile.zapService = payload.zapService;
-    if (payload.website) profile.website = payload.website;
+    Object.keys(payload).forEach((key) => {
+        switch (key) {
+            case "name":
+                profile.name = payload.name;
+                break;
+            case "display_name":
+            case "displayName":
+                profile.displayName = payload.displayName || payload.display_name;
+                break;
+            case "image":
+            case "picture":
+                profile.image = payload.image || payload.picture;
+                break;
+            case "banner":
+                profile.banner = payload.banner;
+                break;
+            case "bio":
+                profile.bio = payload.bio;
+                break;
+            case "nip05":
+                profile.nip05 = payload.nip05;
+                break;
+            case "lud06":
+                profile.lud06 = payload.lud06;
+                break;
+            case "lud16":
+                profile.lud16 = payload.lud16;
+                break;
+            case "about":
+                profile.about = payload.about;
+                break;
+            case "zapService":
+                profile.zapService = payload.zapService;
+                break;
+            case "website":
+                profile.website = payload.website;
+                break;
+            default:
+                profile[key] = payload[key];
+                break;
+        }
+    });
 
     return profile;
 }

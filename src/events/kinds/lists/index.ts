54497b3d7648f2673da3f3aab4d98a271ab58a62
pablof7z <p@f7z.io>
2023-08-18 22:09:48 +0300

    improve code

--- src/events/kinds/lists/index.ts
+++ src/events/kinds/lists/index.ts
@@ -49,13 +49,14 @@ export class NDKList extends NDKEvent {
      * Returns the name of the list.
      */
     get name(): string | undefined {
-      if(this.kind === NDKKind.MuteList && !this.tagValue("name")) {
-        return "Mute";
-      } else if (this.kind === NDKKind.PinList && !this.tagValue("name")) {
-        return "Pin";
-      } else {
-        return this.tagValue("name") ?? this.tagValue("d");
-      }
+        const nameTag = this.tagValue("name");
+        if (this.kind === NDKKind.MuteList && !nameTag) {
+            return "Mute";
+        } else if (this.kind === NDKKind.PinList && !nameTag) {
+            return "Pin";
+        } else {
+            return nameTag ?? this.tagValue("d");
+        }
     }
 
     /**

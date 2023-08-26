f88f663c17d6644663811406633b7e8d65fbaca1
triesap <137732411+triesap@users.noreply.github.com>
2023-08-26 08:23:13 +0000

    configure build process

---
+++ ndk/src/light-bolt11-decoder.d.ts
@@ -0,0 +1,3 @@
+declare module "light-bolt11-decoder" {
+    export function decode(bolt11: string): any;
+}
--- packages/ndk-svelte-components/.storybook/manager.ts
+++
@@ -1,6 +0,0 @@
-import { addons } from "@storybook/manager-api";
-import ndkTheme from "./ndk-theme";
-
-addons.setConfig({
-    theme: ndkTheme,
-});

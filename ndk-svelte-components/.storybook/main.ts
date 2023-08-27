d130309f16310018cc5d1172e71be7806d1ec720
triesap <137732411+triesap@users.noreply.github.com>
2023-08-27 18:36:59 +0000

    add `ndk-svelte-components` justfile, init storybook

---
+++ ndk-svelte-components/.storybook/main.ts
@@ -0,0 +1,26 @@
+import type { StorybookConfig } from "@storybook/sveltekit";
+import { join, dirname } from "path";
+
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value: string): any {
+    return dirname(require.resolve(join(value, "package.json")));
+}
+const config: StorybookConfig = {
+    stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+    addons: [
+        getAbsolutePath("@storybook/addon-links"),
+        getAbsolutePath("@storybook/addon-essentials"),
+        getAbsolutePath("@storybook/addon-interactions"),
+    ],
+    framework: {
+        name: getAbsolutePath("@storybook/sveltekit"),
+        options: {},
+    },
+    docs: {
+        autodocs: "tag",
+    },
+};
+export default config;

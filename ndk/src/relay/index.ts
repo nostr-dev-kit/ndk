c4040ce99c12634641229beb8f0cad9eb82fc480
Pablo Fernandez <p@f7z.io>
2023-08-29 10:52:57 +0300

    remove console.log

--- ndk/src/relay/index.ts
+++ ndk/src/relay/index.ts
@@ -159,7 +159,6 @@ export class NDKRelay extends EventEmitter {
             this.updateConnectionStats.attempt();
             this._status = NDKRelayStatus.CONNECTING;
             await this.relay.connect();
-            console.log(`after this.relay.connect`);
         } catch (e) {
             this.debug("Failed to connect", e);
             this._status = NDKRelayStatus.DISCONNECTED;

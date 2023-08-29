6be7dc5eb62d647cb3c746e9ff77f803a554850c
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-08-29 17:55:17 +0200

    more storybook migration

--- ndk-svelte-components/.storybook/main.ts
+++ ndk-svelte-components/.storybook/main.ts
@@ -14,11 +14,11 @@ const config: StorybookConfig = {
         getAbsolutePath("@storybook/addon-links"),
         getAbsolutePath("@storybook/addon-essentials"),
         getAbsolutePath("@storybook/addon-interactions"),
+        getAbsolutePath("@storybook/addon-mdx-gfm"),
         getAbsolutePath("@storybook/addon-mdx-gfm")
     ],
     framework: {
         name: getAbsolutePath("@storybook/sveltekit"),
-        options: {},
     },
     docs: {
         autodocs: "tag",

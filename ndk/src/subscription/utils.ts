b947a8f108db6b700bb08fab4999262ea0f23e94
Pablo Fernandez <p@f7z.io>
2023-09-05 10:53:28 +0300

    Improve subId generation logic

--- ndk/src/subscription/utils.ts
+++ ndk/src/subscription/utils.ts
@@ -27,3 +27,49 @@ function resultHasAllRequestedIds(subscription: NDKSubscription): boolean {
 
     return !!ids && ids.length === subscription.eventFirstSeen.size;
 }
+
+/**
+ * Generates a subscription ID based on the subscriptions and filter.
+ *
+ * When some of the subscriptions specify a subId, those are used,
+ * joining them with a comma.
+ *
+ * If none of the subscriptions specify a subId, a subId is generated
+ * by joining all the filter keys, and expanding the kinds with the requested kinds.
+ */
+export function generateSubId(
+    subscriptions: NDKSubscription[],
+    filters: NDKFilter[]
+): string {
+    const subIds = subscriptions.map(sub => sub.subId).filter(Boolean);
+    let resultingSubId: string = "";
+    const filterNonKindKeys = new Set<string>();
+    const filterKinds = new Set<number>();
+
+    if (subIds.length > 0) {
+        resultingSubId = subIds.join(',');
+    } else {
+        for (const filter of filters) {
+            for (const key of Object.keys(filter)) {
+                if (key === "kinds") {
+                    filter.kinds?.forEach(k => filterKinds.add(k));
+                } else {
+                    filterNonKindKeys.add(key);
+                }
+            }
+        }
+
+        if (filterKinds.size > 0) {
+            resultingSubId = "kinds:" + Array.from(filterKinds).join(',');
+        }
+
+        if (filterNonKindKeys.size > 0) {
+            resultingSubId += '-' + Array.from(filterNonKindKeys).join(',');
+        }
+    }
+
+    // Add the random string to the resulting subId
+    resultingSubId += "-" + Math.floor(Math.random() * 999).toString();
+
+    return resultingSubId;
+}

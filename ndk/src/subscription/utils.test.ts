6445ac20224ae189d29c6257d1ebf708bff1e999
Pablo Fernandez <p@f7z.io>
2023-09-05 11:55:07 +0300

    test generation of subscription id

---
+++ ndk/src/subscription/utils.test.ts
@@ -0,0 +1,27 @@
+import { NDKSubscription } from '.';
+import { NDK } from '../ndk';
+import { generateSubId } from './utils';
+
+const ndk = new NDK();
+
+describe('generateSubId', () => {
+    it("generates a subId based on the subscriptions' subIds", () => {
+        const sub1 = new NDKSubscription(ndk, { kinds: [1] }, { subId: "sub1" });
+        const sub2 = new NDKSubscription(ndk, { authors: ["abc"] }, { subId: "sub2" });
+
+        const subId = generateSubId([sub1, sub2], [{ kinds: [1] }, { authors: ["abc"] } ]);
+
+        expect(subId).toMatch(/sub1,sub2-\d+/);
+    });
+
+    it("generates a subId based on the filters when there is no subId", () => {
+        const sub1Filter = { kinds: [1] };
+        const sub2Filter = { kinds: [2], authors: ["abc"] };
+        const sub1 = new NDKSubscription(ndk, sub1Filter);
+        const sub2 = new NDKSubscription(ndk, sub2Filter);
+
+        const subId = generateSubId([sub1, sub2], [sub1Filter, sub2Filter ]);
+
+        expect(subId).toMatch(/kinds:1,2-authors-\d+/);
+    });
+});

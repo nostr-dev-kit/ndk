ffc78c64e60f87ffb827d263ae8a5a07b3e64eea
Pablo Fernandez <p@f7z.io>
2023-09-05 11:54:51 +0300

    instantiate outbox tracker properly

--- ndk/src/outbox/tracker.test.ts
+++ ndk/src/outbox/tracker.test.ts
@@ -6,7 +6,7 @@ const ndk = new NDK();
 
 describe("OutboxTracker", () => {
     it("increases the reference count when tracking an existing user", () => {
-        const tracker = new OutboxTracker();
+        const tracker = new OutboxTracker(ndk);
         const user = new NDKUser({ hexpubkey: "fa984bd7dbb282f07e16e7ae87b26a2a7b9b90b7246a44771f0cf5ae58018f52" });
         const user2 = new NDKUser({ hexpubkey: "fa984bd7dbb282f07e16e7ae87b26a2a7b9b90b7246a44771f0cf5ae58018f52" });
 

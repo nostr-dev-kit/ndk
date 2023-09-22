41946cb5274b8505c5e4a4b8c66d5ffb019ebc79
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-09-22 23:16:33 +0200

    Swap out dompurify for sanitizeHtml

--- ndk-svelte-components/src/lib/utils/markdown.ts
+++ ndk-svelte-components/src/lib/utils/markdown.ts
@@ -1,13 +1,13 @@
-import DOMPurify from "isomorphic-dompurify";
 import { marked } from "marked";
 import { gfmHeadingId } from "marked-gfm-heading-id";
 import { mangle } from "marked-mangle";
+import sanitizeHtml from "sanitize-html";
 
 export const markdownToHtml = (content: string): string => {
     marked.use(mangle());
     marked.use(gfmHeadingId());
 
-    return DOMPurify.sanitize(
+    return sanitizeHtml(
         // eslint-disable-next-line no-misleading-character-class
         marked.parse(content.replace(/^[\u200B\u200C\u200D\u200E\u200F\uFEFF]/, ""))
     );

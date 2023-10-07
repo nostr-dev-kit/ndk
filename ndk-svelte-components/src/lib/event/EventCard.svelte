ce70b204469a177b4cb8c9636d1810c2bc530993
Pablo Fernandez <p@f7z.io>
2023-10-07 23:26:56 +0300

    event card time

--- ndk-svelte-components/src/lib/event/EventCard.svelte
+++ ndk-svelte-components/src/lib/event/EventCard.svelte
@@ -3,11 +3,13 @@
     import Avatar from '$lib/user/Avatar.svelte';
     import type { NDKEvent } from '@nostr-dev-kit/ndk';
     import type NDK from '@nostr-dev-kit/ndk';
+    import Time from "svelte-time";
 
     export let ndk: NDK;
     export let id: string | undefined = undefined;
     export const relays: string[] | undefined = undefined;
     export let event: NDKEvent | null | undefined = undefined;
+    export let relativeTimeAllowed = true;
 
     // eslint-disable-next-line no-async-promise-executor
     const eventPromise = new Promise(async (resolve, reject) => {
@@ -20,18 +22,39 @@
             else resolve(event);
         }
     });
+
+    export let timeAgoCutoff: number = 60*60*24;
+
+    function useRelativeTime() {
+        if (!relativeTimeAllowed || !event) return false;
+
+        const now = Date.now();
+        const diff = now - event.created_at! * 1000;
+
+        return diff < 1000 * timeAgoCutoff;
+    }
 </script>
 
 {#await eventPromise then}
     <div class="event-card">
         <div class="event-card--header">
-            <Avatar
-                {ndk}
-                user={event?.author}
-                class="event-card--avatar"
-                style="width:40px; height:40px; object-fit:cover; border-radius: 100%; margin-right: 1rem;"
-            />
-            <Name {ndk} user={event?.author} class="event-card--name" />
+            <div class="event-card--header--author">
+                <Avatar
+                    {ndk}
+                    user={event?.author}
+                    class="event-card--avatar"
+                    style="width:40px; height:40px; object-fit:cover; border-radius: 100%; margin-right: 1rem;"
+                />
+                <Name {ndk} user={event?.author} class="event-card--name" />
+            </div>
+
+            <div class="event-card--header--time">
+                <Time
+                    relative={useRelativeTime()}
+                    live={true}
+                    timestamp={event?.created_at * 1000}
+                />
+            </div>
         </div>
         <EventContent {ndk} {event} />
     </div>
@@ -54,6 +77,16 @@
     }
 
     .event-card--header {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: flex-between;
+        gap: 0.25rem;
+        width: 100%;
+        padding: 0;
+    }
+
+    .event-card--header--author {
         display: flex;
         flex-direction: row;
         align-items: center;

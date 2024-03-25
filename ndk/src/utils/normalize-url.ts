6c2eff15ee5dc85826aac23106c6f3325e40dedc
Pablo Fernandez <p@f7z.io>
2024-03-25 11:09:15 +0000

    URL normalization

---
+++ ndk/src/utils/normalize-url.ts
@@ -0,0 +1,20 @@
+import normalizeUrl from 'normalize-url';
+
+export function normalizeRelayUrl(url: string): string {
+    let r = normalizeUrl(url, {
+        stripAuthentication: false,
+        stripWWW: false,
+        stripHash: true,
+    });
+
+    // if it doesn't end with a slash, add it
+    if (!r.endsWith('/')) {
+        r += '/';
+    }
+
+    return r;
+}
+
+export function normalize(urls: string[]): string[] {
+    return urls.map(normalizeRelayUrl);
+}

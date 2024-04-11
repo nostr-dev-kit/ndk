5a254faeed914023b4258b8c7a49464507192e8c
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2024-04-11 12:11:28 +0100

    Add more tests on connectivity

--- ndk/src/relay/connectivity.test.ts
+++ ndk/src/relay/connectivity.test.ts
@@ -5,29 +5,67 @@ describe("NDKRelayConnectivity", () => {
     let ndkRelayConnectivity: NDKRelayConnectivity;
     let ndkRelayMock: NDKRelay;
     let relayConnectSpy: jest.SpyInstance;
-    beforeEach(() => {
+    let relayDisconnectSpy: jest.SpyInstance;
+
+    beforeEach(async () => {
         ndkRelayMock = new NDKRelay("ws://localhost");
         ndkRelayConnectivity = new NDKRelayConnectivity(ndkRelayMock);
         // Mock the connect method on nostr tools relay
         relayConnectSpy = jest.spyOn(ndkRelayConnectivity.relay, "connect").mockResolvedValue();
+        // Mock the close method on the nostr tools relay
+        relayDisconnectSpy = jest.spyOn(ndkRelayConnectivity.relay, "close").mockReturnValue();
+        await ndkRelayConnectivity.connect();
     });
 
     afterEach(() => {
         jest.restoreAllMocks();
     });
 
-    it("connects and disconnects from the relay", async () => {
-        // First connect
-        await ndkRelayConnectivity.connect();
-        expect(relayConnectSpy).toHaveBeenCalled();
-        expect(ndkRelayConnectivity.status).toBe(NDKRelayStatus.CONNECTED);
-        expect(ndkRelayConnectivity.isAvailable()).toBe(true);
-        expect(ndkRelayConnectivity.connectionStats.attempts).toBe(1);
-        expect(ndkRelayConnectivity.connectionStats.connectedAt).toBeDefined();
-        // Then disconnect
-        ndkRelayConnectivity.disconnect();
-        expect(ndkRelayConnectivity.status).toBe(NDKRelayStatus.DISCONNECTING);
-        expect(ndkRelayConnectivity.isAvailable()).toBe(false);
-        // Can't test the connection stats updates without onclose callback firing from relay
-    });
+    describe("connecting", () => {
+        it("calls connect() on the nostr-tools AbstractRelay instance", () => {
+            expect(relayConnectSpy).toHaveBeenCalled();
+        });
+    
+        it("updates connected status properly", () => {
+            // Check that we updated our status
+            expect(ndkRelayConnectivity.status).toBe(NDKRelayStatus.CONNECTED);
+            // Check that we're available
+            expect(ndkRelayConnectivity.isAvailable()).toBe(true);
+        });
+    
+        it("updates connectionStats on connect", () => {
+            expect(ndkRelayConnectivity.connectionStats.attempts).toBe(1);
+            expect(ndkRelayConnectivity.connectionStats.connectedAt).toBeDefined();
+        });
+    })
+
+    // TODO: Test auth
+
+    describe("disconnecting", () => {
+        beforeEach(() => {
+            ndkRelayConnectivity.disconnect();
+        })
+
+        it("disconnects from the relay", async () => {
+            expect(relayDisconnectSpy).toHaveBeenCalled();
+        });
+
+        it("updates connected status properly", () => {
+            expect(ndkRelayConnectivity.status).toBe(NDKRelayStatus.DISCONNECTING);
+            expect(ndkRelayConnectivity.isAvailable()).toBe(false);
+        })
+
+        // Test that onclose callback was properly called
+        it.skip("updates the connectionStats for disconnect", () => {
+            expect(ndkRelayConnectivity.connectionStats.connectedAt).toBe(undefined);
+            expect(ndkRelayConnectivity.connectionStats.durations.length).toBe(1);
+        })
+
+        // TODO: Can we test the emit on NDKRelay?
+        // TODO: Test reconnection logic (disconnect called from AbstractRelay)
+    })
+
+    
+
+    
 });

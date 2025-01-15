19ebda07cb8ad82d579524c5250c82f63a2167c2
J the Code Monkey <jaythecodemonkey.7vk7i@slmail.me>
2025-01-15 16:59:53 -0500

    WIP: remove unused nip-04 queue code

--- ndk-mobile/src/signers/nip55.ts
+++ ndk-mobile/src/signers/nip55.ts
@@ -1,5 +1,3 @@
-import debug from "debug";
-
 import {
     type NostrEvent,
     type Npub,
@@ -20,14 +18,6 @@ type NDKNip55Permissions = {
     kind?: number;
 };
 
-// type Nip04QueueItem = {
-//     type: "encrypt" | "decrypt";
-//     counterpartyHexpubkey: string;
-//     value: string;
-//     resolve: (value: string) => void;
-//     reject: (reason?: Error) => void;
-// };
-
 type GetPublicKeyResult = {
     pubkey?: Hexpubkey;
     canceled?: boolean;
@@ -46,13 +36,6 @@ type IntentExtra = {
 export class NDKNip55Signer implements NDKSigner {
     private _userPromise: Promise<NDKUser> | undefined;
     private _user: NDKUser | undefined;
-    // public nip04Queue: Nip04QueueItem[] = [];
-    // private nip04Processing = false;
-    private debug: debug.Debugger;
-
-    public constructor() {
-        this.debug = debug("ndk:nip55");
-    }
 
     public async blockUntilReady(permissions?: NDKNip55Permissions[]): Promise<NDKUser> {
         // TODO
@@ -276,9 +259,6 @@ export class NDKNip55Signer implements NDKSigner {
                 if (intentExtra.result === "Could not decrypt the message") {
                     throw new Error("Encryption failed");
                 }
-                // TODO
-                // Look into if we need a queue like for NIP-07
-                // return this.queueNip04("encrypt", recipientHexPubKey, value);
                 return intentExtra.result;
                 // If the activity operation was canceled, error out
             } else if (intent.resultCode === 0) {
@@ -325,9 +305,6 @@ export class NDKNip55Signer implements NDKSigner {
                 if (intentExtra.result === "Could not decrypt the message") {
                     throw new Error("Decryption failed");
                 }
-                // TODO
-                // Look into if we need a queue like for NIP-07
-                // return this.queueNip04("decrypt", senderHexPubKey, value);
                 return intentExtra.result;
                 // If the activity operation was canceled, error out
             } else if (intent.resultCode === 0) {
@@ -342,70 +319,6 @@ export class NDKNip55Signer implements NDKSigner {
             throw error;
         }
     }
-
-    // private async queueNip04(
-    //     type: "encrypt" | "decrypt",
-    //     counterpartyHexpubkey: string,
-    //     value: string
-    // ): Promise<string> {
-    //     return new Promise((resolve, reject) => {
-    //         this.nip04Queue.push({
-    //             type,
-    //             counterpartyHexpubkey,
-    //             value,
-    //             resolve,
-    //             reject,
-    //         });
-    //
-    //         if (!this.nip04Processing) {
-    //             this.processNip04Queue();
-    //         }
-    //     });
-    // }
-
-    // private async processNip04Queue(item?: Nip04QueueItem, retries = 0): Promise<void> {
-    //     if (!item && this.nip04Queue.length === 0) {
-    //         this.nip04Processing = false;
-    //         return;
-    //     }
-    //
-    //     this.nip04Processing = true;
-    //     const { type, counterpartyHexpubkey, value, resolve, reject } =
-    //         item || this.nip04Queue.shift()!;
-    //
-    //     try {
-    //         let result;
-    //
-    //         if (type === "encrypt") {
-    //             result = await window.nostr!.nip04!.encrypt(counterpartyHexpubkey, value);
-    //         } else {
-    //             result = await window.nostr!.nip04!.decrypt(counterpartyHexpubkey, value);
-    //         }
-    //
-    //         resolve(result);
-    //         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    //     } catch (error: any) {
-    //         // retry a few times if the call is already executing
-    //         if (error.message && error.message.includes("call already executing")) {
-    //             if (retries < 5) {
-    //                 this.debug("Retrying encryption queue item", {
-    //                     type,
-    //                     counterpartyHexpubkey,
-    //                     value,
-    //                     retries,
-    //                 });
-    //                 setTimeout(() => {
-    //                     this.processNip04Queue(item, retries + 1);
-    //                 }, 50 * retries);
-    //
-    //                 return;
-    //             }
-    //         }
-    //         reject(error);
-    //     }
-    //
-    //     this.processNip04Queue();
-    // }
 
     private convertNpubToHex(npub: Npub): Hexpubkey {
         try {

16c6c7dd8be6d5cbcc9a37f3fc30b430671d644b
pablof7z <pfer@me.com>
2025-10-30 00:26:54 +0200

    fix: restore ?raw import for repost-action builder example
    - Fixed build error by reverting to ?raw import instead of template string
    - Svelte parser was getting confused by <script> tag in template string

--- svelte/registry/src/routes/components/repost-action/+page.svelte
+++ svelte/registry/src/routes/components/repost-action/+page.svelte
@@ -9,21 +9,7 @@
   import BasicExampleRaw from './examples/repost-action-basic.svelte?raw';
 
   import BuilderExample from './examples/repost-action-builder.svelte';
-
-  const BuilderExampleRaw = `<script lang="ts">
-  import { createRepostAction } from '@nostr-dev-kit/svelte';
-
-  const repostState = createRepostAction(() => ({ event }), ndk);
-</script>
-
-<div class="flex gap-2 pt-4 border-t border-border">
-  <button
-    onclick={repostState.repost}
-    disabled={repostState.hasReposted}
-  >
-    🔄 Repost ({repostState.count} times)
-  </button>
-</div>`;
+  import BuilderExampleRaw from './examples/repost-action-builder__code.svelte?raw';
 
   const ndk = getContext<NDKSvelte>('ndk');
 

f133285ac298705b54dca8716b19c43750078dc3
pablof7z <pfer@me.com>
2025-11-02 08:17:19 +0200

    feat: add embedded-handlers convenience import
    Single-file import to register all embedded event handlers.
    
    Usage:
      import '$lib/registry/components/embedded-handlers';
      // Registers: article, highlight, note handlers
    
    Equivalent to:
      import '$lib/registry/components/article-embedded';
      import '$lib/registry/components/highlight-embedded';
      import '$lib/registry/components/note-embedded';
    
    This provides the most convenient way for users to enable
    all default embedded event rendering with a single line.
    
    Individual handlers can still be imported separately for
    more granular control over which handlers are loaded.

---
+++ svelte/registry/src/lib/registry/components/embedded-handlers.ts
@@ -0,0 +1,23 @@
+/**
+ * Convenience import for all embedded event handlers
+ *
+ * Importing this file registers all default embedded handlers with ContentRenderer.
+ * Handlers are located directly under components/ (not nested in event/content/).
+ *
+ * @example
+ * ```ts
+ * import '$lib/registry/components/embedded-handlers';
+ * // Now all embedded events render with their respective handlers
+ * ```
+ *
+ * Individual handlers can also be imported separately:
+ * ```ts
+ * import '$lib/registry/components/article-embedded';
+ * import '$lib/registry/components/highlight-embedded';
+ * import '$lib/registry/components/note-embedded';
+ * ```
+ */
+
+import './article-embedded';
+import './highlight-embedded';
+import './note-embedded';

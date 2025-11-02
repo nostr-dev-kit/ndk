0d63f684c86f5df828ac3074ec7af7e9dd589dd0
pablof7z <pfer@me.com>
2025-11-02 19:09:02 +0200

    fix: correct import paths in blocks/index.ts
    All blocks are located in ../components/ subdirectories, not in ./
    Update all exports to use correct relative paths from blocks/ to components/
    
    This resolves hundreds of 'Cannot find module' errors.

--- svelte/registry/src/lib/registry/blocks/index.ts
+++ svelte/registry/src/lib/registry/blocks/index.ts
@@ -6,77 +6,75 @@
  */
 
 // Article Cards
-export { default as ArticleCardHero } from './article-card-hero.svelte';
-export { default as ArticleCardMedium } from './article-card-medium.svelte';
-export { default as ArticleCardNeon } from './article-card-neon.svelte';
-export { default as ArticleCardPortrait } from './article-card-portrait.svelte';
+export { default as ArticleCardHero } from '../components/article-card/article-card-hero.svelte';
+export { default as ArticleCardMedium } from '../components/article-card/article-card-medium.svelte';
+export { default as ArticleCardNeon } from '../components/article-card/article-card-neon.svelte';
+export { default as ArticleCardPortrait } from '../components/article-card/article-card-portrait.svelte';
 
 // Event Cards
-export { default as EventCardClassic } from './event-card-classic.svelte';
-export { default as EventCardMenu } from './event-card-menu.svelte';
+export { default as EventCardClassic } from '../components/event-card/event-card-classic.svelte';
+export { default as EventCardMenu } from '../components/event-card/event-card-menu.svelte';
 
 // Follow Components
-export { default as FollowButton } from './follow-button.svelte';
-export { default as FollowButtonAnimated } from './follow-button-animated.svelte';
-export { default as FollowButtonPill } from './follow-button-pill.svelte';
-export { default as FollowPackCompact } from './follow-pack-compact.svelte';
-export { default as FollowPackHero } from './follow-pack-hero.svelte';
-export { default as FollowPackListItem } from './follow-pack-list-item.svelte';
-export { default as FollowPackModernPortrait } from './follow-pack-modern-portrait.svelte';
-export { default as FollowPackPortrait } from './follow-pack-portrait.svelte';
+export { default as FollowButton } from '../components/actions/follow-button.svelte';
+export { default as FollowButtonAnimated } from '../components/actions/follow-button-animated.svelte';
+export { default as FollowButtonPill } from '../components/actions/follow-button-pill.svelte';
+export { default as FollowPackCompact } from '../components/follow-pack/follow-pack-compact.svelte';
+export { default as FollowPackHero } from '../components/follow-pack/follow-pack-hero.svelte';
+export { default as FollowPackListItem } from '../components/follow-pack/follow-pack-list-item.svelte';
+export { default as FollowPackModernPortrait } from '../components/follow-pack/follow-pack-modern-portrait.svelte';
+export { default as FollowPackPortrait } from '../components/follow-pack/follow-pack-portrait.svelte';
 
 // Highlight Cards
-export { default as HighlightCardCompact } from './highlight-card-compact.svelte';
-export { default as HighlightCardElegant } from './highlight-card-elegant.svelte';
-export { default as HighlightCardFeed } from './highlight-card-feed.svelte';
-export { default as HighlightCardGrid } from './highlight-card-grid.svelte';
+export { default as HighlightCardCompact } from '../components/highlight-card/highlight-card-compact.svelte';
+export { default as HighlightCardElegant } from '../components/highlight-card/highlight-card-elegant.svelte';
+export { default as HighlightCardFeed } from '../components/highlight-card/highlight-card-feed.svelte';
+export { default as HighlightCardGrid } from '../components/highlight-card/highlight-card-grid.svelte';
 
 // Image Cards
-export { default as ImageCard } from './image-card.svelte';
-export { default as ImageCardHero } from './image-card-hero.svelte';
-export { default as ImageCardInstagram } from './image-card-instagram.svelte';
+export { default as ImageCard } from '../components/image-card/image-card.svelte';
+export { default as ImageCardHero } from '../components/image-card/image-card-hero.svelte';
+export { default as ImageCardInstagram } from '../components/image-card/image-card-instagram.svelte';
 
 // Login
 export { default as LoginCompact } from './login-compact.svelte';
 
 // Media Upload
-export { default as MediaUploadCarousel } from './media-upload-carousel.svelte';
-export { default as UploadButton } from './upload-button.svelte';
+export { default as MediaUploadCarousel } from '../components/media-upload-carousel/media-upload-carousel.svelte';
+export { default as UploadButton } from '../components/actions/upload-button.svelte';
 
 // Mention
-export { default as MentionModern } from './mention-modern.svelte';
+export { default as MentionModern } from '../components/mention-modern/mention-modern.svelte';
 
 // Actions
-export { default as MuteButton } from './mute-button.svelte';
-export { default as ReactionButton } from './reaction-button.svelte';
-export { default as ReactionEmojiButton } from './reaction-emoji-button.svelte';
-export { default as ReactionSlack } from './reaction-slack.svelte';
-export { default as RepostButton } from './repost-button.svelte';
-export { default as RepostButtonPill } from './repost-button-pill.svelte';
+export { default as MuteButton } from '../components/actions/mute-button.svelte';
+export { default as ReactionButton } from '../components/actions/reaction-button.svelte';
+export { default as ReactionEmojiButton } from '../components/actions/reaction-emoji-button.svelte';
+export { default as ReactionSlack } from '../components/actions/reaction-slack.svelte';
+export { default as RepostButton } from '../components/actions/repost-button.svelte';
+export { default as RepostButtonPill } from '../components/actions/repost-button-pill.svelte';
 
 // Note Composer
-export { default as NoteComposerCard } from './note-composer-card.svelte';
-export { default as NoteComposerInline } from './note-composer-inline.svelte';
-export { default as NoteComposerMinimal } from './note-composer-minimal.svelte';
-export { default as NoteComposerModal } from './note-composer-modal.svelte';
+export { default as NoteComposerCard } from '../components/note-composer/note-composer-card.svelte';
+export { default as NoteComposerInline } from '../components/note-composer/note-composer-inline.svelte';
+export { default as NoteComposerMinimal } from '../components/note-composer/note-composer-minimal.svelte';
+export { default as NoteComposerModal } from '../components/note-composer/note-composer-modal.svelte';
 
 // Relay Components
-export { default as RelayInputBlock } from './relay-input-block.svelte';
-export { default as RelaySelectorInline } from './relay-selector-inline.svelte';
-export { default as RelaySelectorPopover } from './relay-selector-popover.svelte';
+export { default as RelaySelectorPopover } from '../components/relay-selector/relay-selector-popover.svelte';
 
 // Thread View
 export { default as ThreadViewTwitter } from './thread-view-twitter.svelte';
 
 // User Components
-export { default as UserCardClassic } from './user-card-classic.svelte';
-export { default as UserCardCompact } from './user-card-compact.svelte';
-export { default as UserCardLandscape } from './user-card-landscape.svelte';
-export { default as UserCardPortrait } from './user-card-portrait.svelte';
-export { default as UserListItem } from './user-list-item.svelte';
-export { default as UserProfileHero } from './user-profile-hero.svelte';
-export { default as UserSearchCombobox } from './user-search-combobox.svelte';
+export { default as UserCardClassic } from '../components/user-card/user-card-classic.svelte';
+export { default as UserCardCompact } from '../components/user-card/user-card-compact.svelte';
+export { default as UserCardLandscape } from '../components/user-card/user-card-landscape.svelte';
+export { default as UserCardPortrait } from '../components/user-card/user-card-portrait.svelte';
+export { default as UserListItem } from '../components/user-card/user-list-item.svelte';
+export { default as UserProfileHero } from '../components/user-profile/user-profile-hero.svelte';
+export { default as UserSearchCombobox } from '../components/user-search-combobox/user-search-combobox.svelte';
 
 // Voice Message Cards
-export { default as VoiceMessageCardCompact } from './voice-message-card-compact.svelte';
-export { default as VoiceMessageCardExpanded } from './voice-message-card-expanded.svelte';
+export { default as VoiceMessageCardCompact } from '../components/voice-message-card/voice-message-card-compact.svelte';
+export { default as VoiceMessageCardExpanded } from '../components/voice-message-card/voice-message-card-expanded.svelte';

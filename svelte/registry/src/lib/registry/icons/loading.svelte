5da33932c838d198264a245d3cb6901bf1ba5346
pablof7z <pfer@me.com>
2025-11-04 20:23:47 +0200

    fix(svelte): loading.svelte - remove self-closing non-void element tags

--- svelte/registry/src/lib/registry/icons/loading.svelte
+++ svelte/registry/src/lib/registry/icons/loading.svelte
@@ -18,47 +18,47 @@
     stroke="currentColor"
     stroke-linecap="round"
     stroke-width="1.5"
-  />
+  ></path>
   <path
     d="M12 18V21"
     stroke="currentColor"
     stroke-linecap="round"
     stroke-width="1.5"
-  />
+  ></path>
   <path
     d="M21 12L18 12"
     stroke="currentColor"
     stroke-linecap="round"
     stroke-width="1.5"
-  />
+  ></path>
   <path
     d="M6 12L3 12"
     stroke="currentColor"
     stroke-linecap="round"
     stroke-width="1.5"
-  />
+  ></path>
   <path
     d="M18.3635 5.63672L16.2422 7.75804"
     stroke="currentColor"
     stroke-linecap="round"
     stroke-width="1.5"
-  />
+  ></path>
   <path
     d="M7.75804 16.2422L5.63672 18.3635"
     stroke="currentColor"
     stroke-linecap="round"
     stroke-width="1.5"
-  />
+  ></path>
   <path
     d="M18.3635 18.3635L16.2422 16.2422"
     stroke="currentColor"
     stroke-linecap="round"
     stroke-width="1.5"
-  />
+  ></path>
   <path
     d="M7.75804 7.75804L5.63672 5.63672"
     stroke="currentColor"
     stroke-linecap="round"
     stroke-width="1.5"
-  />
+  ></path>
 </svg>

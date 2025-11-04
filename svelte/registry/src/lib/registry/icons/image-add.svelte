2b0a618994aab1b2266b75ec6d3221a9948ad982
pablof7z <pfer@me.com>
2025-11-04 20:23:47 +0200

    fix(svelte): image-add.svelte - remove self-closing non-void element tags

--- svelte/registry/src/lib/registry/icons/image-add.svelte
+++ svelte/registry/src/lib/registry/icons/image-add.svelte
@@ -19,25 +19,25 @@
     stroke-width="1.5"
     stroke-linecap="round"
     stroke-linejoin="round"
-  />
+  ></path>
   <path
     d="M17.5 6.5H22.5M20 4V9"
     stroke="currentColor"
     stroke-width="1.5"
     stroke-linecap="round"
-  />
+  ></path>
   <path
     d="M2.67004 18.9501L7.60004 15.6401C8.39004 15.1101 9.53004 15.1701 10.24 15.7801L10.57 16.0701C11.35 16.7401 12.61 16.7401 13.39 16.0701L17.55 12.5001C18.33 11.8301 19.59 11.8301 20.37 12.5001L22 13.9001"
     stroke="currentColor"
     stroke-width="1.5"
     stroke-linecap="round"
     stroke-linejoin="round"
-  />
+  ></path>
   <path
     d="M9 10C10.1046 10 11 9.10457 11 8C11 6.89543 10.1046 6 9 6C7.89543 6 7 6.89543 7 8C7 9.10457 7.89543 10 9 10Z"
     stroke="currentColor"
     stroke-width="1.5"
     stroke-linecap="round"
     stroke-linejoin="round"
-  />
+  ></path>
 </svg>

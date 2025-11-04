bd91bbcf461653ef61929c1ce94f578366e6d662
pablof7z <pfer@me.com>
2025-11-04 20:23:46 +0200

    fix(svelte): article-card-portrait.svelte - remove self-closing non-void element tags

--- svelte/registry/src/lib/registry/components/article-card/article-card-portrait.svelte
+++ svelte/registry/src/lib/registry/components/article-card/article-card-portrait.svelte
@@ -82,7 +82,7 @@
         <div class="flex items-center justify-between">
           <ReadingTime class="text-xs" />
           <svg width="16" height="16" class="text-primary opacity-0 group-hover:opacity-100 transition-opacity" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" ></path>
           </svg>
         </div>
       </div>

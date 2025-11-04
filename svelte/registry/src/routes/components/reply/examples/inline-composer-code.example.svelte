99e50fe5d89342ca233fd65de3dc69503f11b1c9
pablof7z <pfer@me.com>
2025-11-04 20:23:49 +0200

    fix(svelte): inline-composer-code.example.svelte - remove self-closing non-void element tags

--- svelte/registry/src/routes/components/reply/examples/inline-composer-code.example.svelte
+++ svelte/registry/src/routes/components/reply/examples/inline-composer-code.example.svelte
@@ -56,7 +56,7 @@
         stroke-linejoin="round"
         stroke-width="2"
         d="M3 10h10a8 8 0 018 8v2M3 10l6 6m-6-6l6-6"
-      />
+      ></path>
     </svg>
     <span class="text-sm">{replyAction.count}</span>
   </button>

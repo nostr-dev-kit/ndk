b0a354fff08dc9cacd51548ac49af62a4c66fd12
pablof7z <pfer@me.com>
2025-11-04 20:23:47 +0200

    fix(svelte): relay-card.svelte - remove self-closing non-void element tags

--- svelte/registry/src/lib/registry/components/relay-card/relay-card.svelte
+++ svelte/registry/src/lib/registry/components/relay-card/relay-card.svelte
@@ -131,7 +131,7 @@
           stroke-linecap="round"
           stroke-linejoin="round"
         >
-          <path d="m6 9 6 6 6-6" />
+          <path d="m6 9 6 6 6-6" ></path>
         </svg>
       </button>
     </div>

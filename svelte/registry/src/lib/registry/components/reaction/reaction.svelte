06a54788a003c0889612097dae227b097708d208
pablof7z <pfer@me.com>
2025-11-04 20:23:47 +0200

    fix(svelte): reaction.svelte - remove self-closing non-void element tags

--- svelte/registry/src/lib/registry/components/reaction/reaction.svelte
+++ svelte/registry/src/lib/registry/components/reaction/reaction.svelte
@@ -137,7 +137,7 @@
           color="currentColor"
           fill={stats.hasReacted ? 'currentColor' : 'none'}
         >
-          <path d="M10.4107 19.9677C7.58942 17.858 2 13.0348 2 8.69444C2 5.82563 4.10526 3.5 7 3.5C8.5 3.5 10 4 12 6C14 4 15.5 3.5 17 3.5C19.8947 3.5 22 5.82563 22 8.69444C22 13.0348 16.4106 17.858 13.5893 19.9677C12.6399 20.6776 11.3601 20.6776 10.4107 19.9677Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M10.4107 19.9677C7.58942 17.858 2 13.0348 2 8.69444C2 5.82563 4.10526 3.5 7 3.5C8.5 3.5 10 4 12 6C14 4 15.5 3.5 17 3.5C19.8947 3.5 22 5.82563 22 8.69444C22 13.0348 16.4106 17.858 13.5893 19.9677C12.6399 20.6776 11.3601 20.6776 10.4107 19.9677Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" ></path>
         </svg>
 
         {#if showCount && stats.count > 0}

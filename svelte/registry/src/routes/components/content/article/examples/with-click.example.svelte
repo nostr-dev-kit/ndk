5f3b927e326fa71b389c6641219b8eee030ccdcf
pablof7z <pfer@me.com>
2025-11-04 20:23:48 +0200

    fix(svelte): with-click.example.svelte - remove self-closing non-void element tags

--- svelte/registry/src/routes/components/content/article/examples/with-click.example.svelte
+++ svelte/registry/src/routes/components/content/article/examples/with-click.example.svelte
@@ -41,7 +41,7 @@
         class="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2"
       >
         <svg class="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" ></path>
         </svg>
         <span class="sr-only">Close</span>
       </Dialog.Close>
